fa3f5fa6ece5c1931b8b754024444d8c28aafe5a
Farron <fariasamuel2001@gmail.com>
2021-10-11 13:43:47 +0200

    v12

--- pagesphp/collection.php
+++ pagesphp/collection.php
@@ -2,40 +2,21 @@
 <div class="main">   
 
     <?php
-        /* IMPORTATION AUTOMATIQUE DES IMAGES */
+        /* IMPORTATION AUTOMATIQUE DES IMAGES ET INFOS DES MONTRES */
         include 'pagesphp/connexion.php';   
         $req = $pdo->prepare("SELECT * FROM categorie;");
         $req->execute();
         $categorie = $req->fetchAll();
-        for($i=0; $i<count($categorie); $i++) {
-           echo '<h1>EN VITRINE</h1><br><div class="carousel-articles">';       
-            $req = $pdo->prepare("SELECT * FROM montre where numcategorie=1;");
+        for($j=0; $j<count($categorie); $j++) {
+           echo '<h1>'.$categorie[$j]['libellecategorie'].'</h1><br><div class="carousel-articles">';       
+            $req = $pdo->prepare("SELECT * FROM montre where numcategorie=".$categorie[$j]['numcategorie']);
             $req->execute();
             $montre = $req->fetchAll();
             for($i=0; $i<count($montre); $i++) {
                // var_dump($montre[$i]);
     ?>
-    <!-- IMPORTATION AUTOMATIQUE DES INFOS SUR LES MONTRES -->
     <a class="carousel-article">
-    <img src="images/montre<?=($i+1)?>.webp">
-    <div class="info">
-        <h2><?= $montre[$i]['libellemontre'] ?></h2>
-        <h1><?= $montre[$i]['prixmontre'] ?>€</h1>
-        <h3><?= $montre[$i]['descrptionmontre'] ?></h3>
-    </div>
-    </a>
-    <?php
-        }
-        echo '</div>';
-        echo '<h1>CLASSIQUES</h1><br><div class="carousel-articles">';       
-        $req = $pdo->prepare("SELECT * FROM montre where numcategorie=2;");
-        $req->execute();
-        $montre = $req->fetchAll();
-        for($i=0; $i<count($montre); $i++) {
-            // var_dump($montre[$i]);
-    ?>
-    <a class="carousel-article">
-    <img src="images/montre<?=($i+1)?>.webp">
+    <img src="images/montre<?= ($j==1)?($i+5):($i+1)?>.webp">
     <div class="info">
         <h2><?= $montre[$i]['libellemontre'] ?></h2>
         <h1><?= $montre[$i]['prixmontre'] ?>€</h1>
